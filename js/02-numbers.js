9106b4bf449438f1bb14dd47df48ff3b6bcb9c6a
Kestas <kestutisjankevicius9@gmail.com>
2023-05-29 15:05:16 +0300

    update

--- js/02-numbers.js
+++ js/02-numbers.js
@@ -1,2 +1,34 @@
 console.clear();
 
+/* 
+NUMBERS - skaiciai
+
+Sveikieji/desimtainiai
+Teigiami/neigiami
+Mistiniai - Begalybes / NaN (not a number)
+
+--
+
+Aritmetiniai operatoriai:
++, -, *, / 
+*/
+
+console.log(1);
+console.log(2);
+console.log(3.14);
+console.log(2.72);
+//-----
+console.log(-1);
+console.log(-2);
+//----
+console.log(Infinity);
+console.log(-Infinity);
+//----
+console.log(NaN);
+
+// Kintamuju iniciavimo budai:
+// const - default
+// let - naudoti, tik jei reikia keisti reiksmes
+// var - nenaudoti nebent zinai ka darai
+
+const amzius = 20;
