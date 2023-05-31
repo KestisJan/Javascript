a7e8ecf786093917679ec276ebb2214576e05830
Kestas <kestutisjankevicius9@gmail.com>
2023-05-31 14:01:31 +0300

    update

--- js/03-strings.js
+++ js/03-strings.js
@@ -5,23 +5,83 @@ Iniciavimo budai:
 - viengubos kabutes ('')
 - dvigubos kabutes ("")
 
+
+Ilgis:
+- length 
+
 */
 
 const vardas = 'Jonas';
 const vardas1 = "Maryte";
 const vardas2 = "Maryte";
+////----------------------------
 
-console.log(vardas);
-console.log(vardas1);
-console.log(vardas2);
 
-let num1 = 2;
-let num2 = 3;
-let sum = num1 * num2;
+
+
+
+
+let num = 2;
+let num1 = 1;
+let num2 = 2;
+let num3 = 3;
+let num4 = 4;
+let num5 = 5;
+let num6 = 6;
+let num7 = 7;
+let num8 = 8;
+let num9 = 9;
+let num10 = 10;
+
 
 // With - ("")
-const lentele1 = num1 + " * " + num2  + " = " + sum;
-console.log(lentele1);
+
+
+ 
 // With - (``) 
-const lentele2 = `${num1} * ${num2} = ${sum}`;
+
+let num00 = 5;
+
+let sum2 = num1 * num2;
+const lentele2 = `${num2} * ${num4} = ${sum2}`;
 console.log(lentele2);
+
+
+const multiplier = 3;
+let number = 0;
+const lentele3 = `
+${multiplier} * ${++number} = ${multiplier * number}
+${multiplier} * ${++number} = ${multiplier * number}
+${multiplier} * ${++number} = ${multiplier * number}
+${multiplier} * ${++number} = ${multiplier * number}
+${multiplier} * ${++number} = ${multiplier * number}
+${multiplier} * ${++number} = ${multiplier * number}
+${multiplier} * ${++number} = ${multiplier * number}
+${multiplier} * ${++number} = ${multiplier * number}
+${multiplier} * ${++number} = ${multiplier * number}`
+
+
+
+
+
+console.log(lentele3)
+
+
+const lentele4 =`
+${multiplier} * 1 = ${multiplier * 1}`;
+
+console.log(lentele4);
+
+const str = '       ';
+console.log(str, 8, '>>>', str.lenght);
+
+console.clear();
+
+const userFirstName = 'Chuck';
+const userLastName = 'Norris';
+
+const userAbbr = `${userFirstName[0]}.${userLastName[0]}.` 
+
+console.log(userAbbr);
+
+
